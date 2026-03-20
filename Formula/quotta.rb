7ce0a9eb7c006727bb3bba868f20788b84a96b3e
NaNomicon <lecongnhan29@gmail.com>
2026-03-20 22:08:17 +0700

    fix: correct quotta v0.1.0 formula checksum

--- Formula/quotta.rb
+++ Formula/quotta.rb
@@ -2,7 +2,7 @@ class Quotta < Formula
   desc "CLI for checking quota usage across providers"
   homepage "https://github.com/NaNomicon/quotta"
   url "https://github.com/NaNomicon/quotta/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6f6e8b372e7552d5961ad3b3504b847dfe843376e17fa25a9b6a591676cc926b"
+  sha256 "2fd95f1590580a07964efb253693b722ce533dbaa5ab6111e21ba2e36a5be180"
   license "MIT"
 
   depends_on "go" => :build
